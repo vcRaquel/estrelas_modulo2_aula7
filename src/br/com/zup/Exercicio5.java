b9fc5fe0d527e4f36415ffb6cd36df01ade7022e
Raquel Leão <vc.raquel@gmail.com>
2021-09-01 17:12:27 -0300

    exercicio 5

---
+++ src/br/com/zup/Exercicio5.java
@@ -0,0 +1,26 @@
+package br.com.zup;
+
+import java.util.Scanner;
+
+public class Exercicio5 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int quantidadeNumeros = 5;
+        int contador;
+        double numeroLido = 0;
+        double maiorValor = 0;
+
+        for (contador = 1; contador <= quantidadeNumeros; contador += 1) {
+
+            System.out.println(" Por favor digite o " + contador + " número" );
+            numeroLido = leitor.nextDouble();
+
+            if (contador == 1) {
+                maiorValor = numeroLido;
+            } else if (contador > 1 && numeroLido > maiorValor) {
+                maiorValor = numeroLido;
+            }
+        }
+        System.out.println("O maior número digitado foi: " + maiorValor);
+    }
+}
