1d949ef2fb4ea00815a69bb106047eae93ec5734
Raquel Leão <vc.raquel@gmail.com>
2021-09-01 17:27:15 -0300

    exercicio 6

---
+++ src/br/com/zup/Exercicio6.java
@@ -0,0 +1,12 @@
+package br.com.zup;
+
+public class Exercicio6 {
+    public static void main(String[] args) {
+
+        for (int contador = 1; contador <= 50; contador++) {
+            if (contador % 2 != 0) {
+                System.out.println(contador);
+            }
+        }
+    }
+}
