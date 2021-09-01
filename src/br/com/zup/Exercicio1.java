477aeba77f691a4147ea59b9b27bafc58edd2f1b
Raquel Leão <vc.raquel@gmail.com>
2021-09-01 15:43:49 -0300

    exercicio 1

---
+++ src/br/com/zup/Exercicio1.java
@@ -0,0 +1,26 @@
+package br.com.zup;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        int quantidadeProdutos = 3;
+        int contador;
+        double preco = 0;
+        double menorValor = 0;
+
+        for (contador = 1; contador <= quantidadeProdutos; contador += 1) {
+            System.out.println(" Por favor digite o valor do produto " + contador);
+            preco = leitor.nextDouble();
+
+            if (contador == 1) {
+                menorValor = preco;
+            } else if (contador > 1 && preco < menorValor) {
+                menorValor = preco;
+            }
+        }
+        System.out.println("O menor valor foi o de: R$ " + menorValor);
+    }
+}
