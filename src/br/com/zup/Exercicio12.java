c51a22917efe037332fd77ec2ca1f356b0fc7d06
Raquel Leão <vc.raquel@gmail.com>
2021-09-01 22:03:18 -0300

    exercicio 12

---
+++ src/br/com/zup/Exercicio12.java
@@ -0,0 +1,28 @@
+package br.com.zup;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Exercicio12 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int quantidadeNumeros = 5;
+        int soma = 0;
+        int multiplicacao = 1;
+        int numeroLido = 0;
+
+        List<Integer> numeros = new ArrayList<>();
+
+        for (int contador = 1; contador <= quantidadeNumeros; contador += 1){
+            System.out.println("Digite o " + contador + " número: ");
+            numeroLido = leitor.nextInt();
+            numeros.add(numeroLido);
+            soma += numeroLido;
+            multiplicacao *= numeroLido;
+        }
+        System.out.println("Os números digitados foram: " + numeros);
+        System.out.println("A soma desses números é igual a: " + soma);
+        System.out.println("A multiplicação desses números é igual a: " + multiplicacao);
+    }
+}
