cdb324171bc38ef9453314105bd7120ceecbac18
Raquel Leão <vc.raquel@gmail.com>
2021-09-01 23:54:54 -0300

    exercicio level up entrega máxima

--- src/br/com/zup/ExercicioLevelUp.java
+++ src/br/com/zup/ExercicioLevelUp.java
@@ -7,18 +7,39 @@ import java.util.Scanner;
 public class ExercicioLevelUp {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-        int seletor;
-        Map<String, String> alunos = new HashMap<>();
-
-        System.out.println("Favor digitar o e-mail do aluno: ");
-        String email = leitor.nextLine();
-        System.out.println("Favor digitar o nome e o telefone do aluno de acordo com o modelo: ");
-        System.out.println("Nome do Aluno; telefone");
-        String aluno = leitor.nextLine();
-        alunos.put(email, aluno);
+        Scanner leitor2 = new Scanner(System.in);
+        int seletor = 1;
+        Map<String,String> alunos = new HashMap<>();
 
 
-        System.out.println(alunos);
+
+        while (seletor >0 && seletor != 3){
+            System.out.println("Gostaria de cadastrar um aluno?");
+            System.out.println("Digite 1 para sim, 2 remover e 3 para encerrar o programa e sair");
+            seletor = leitor.nextInt();
+            switch (seletor){
+                case 1:
+                    System.out.print("Favor digitar o e-mail do aluno: ");
+                    String email = leitor.next();
+                    System.out.print("Favor digitar o nome e o telefone do aluno de acordo com o modelo: ");
+                    System.out.print("Nome do Aluno; telefone");
+                    String aluno = leitor2.nextLine();
+                    alunos.put(email,aluno);
+                    break;
+                case 2:
+                    System.out.println("Favor digitar o e-mail do aluno a ser removido: ");
+                    email = leitor.next();
+                    alunos.remove(email);
+                    break;
+            }
+        }
+
+        System.out.println("Obrigado por usar o programa!");
+        System.out.println("A lista de alunos cadastrados até o momento é: " + alunos);
+
+
+
+
 
 
     }
